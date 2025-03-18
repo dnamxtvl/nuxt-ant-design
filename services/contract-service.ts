6dcb53deedaa3ac98e224ef97442f77522624b84
NAM Do Van 1 (VTI.D5) <nam.dovan1@vti.vn>
2025-03-18 17:36:33 +0700

    Add logger

--- services/contract-service.ts
+++ services/contract-service.ts
@@ -5,8 +5,8 @@ import type { SearchContractParams } from '~/types/contract/req';
 
 export default class ContractService {
     list = async (params: SearchContractParams) => {
-        const endpoint = API_CONST.CONTRACT.LIST; // TODO: uncomment after integration
-        //const endpoint = DUMMY_API.CONTRACT.LIST; // TODO: remove after integration
+        //const endpoint = API_CONST.CONTRACT.LIST; // TODO: uncomment after integration
+        const endpoint = DUMMY_API.CONTRACT.LIST; // TODO: remove after integration
         return await useCustomFetch(endpoint, { method: "GET", params: params });
     };
 }
